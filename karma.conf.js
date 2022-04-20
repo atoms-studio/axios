65224eaa912425ad08dbb2349eafd4c50f594ed6
Matteo Rigon <matteo.rigon@atoms.studio>
2022-04-20 15:09:15 +0200

    feat: disable check for process prototype

--- karma.conf.js
+++ karma.conf.js
@@ -132,7 +132,7 @@ module.exports = function(config) {
     browsers = ['FirefoxHeadless', 'ChromeHeadless'];
   } else {
     console.log('Running locally since SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are not set.');
-    browsers = ['Firefox', 'Chrome'];
+    browsers = [/* 'Firefox',*/ 'Chrome'];
   }
 
   config.set({
